84d02a43b5cf395d4a98c0c5fe8004e7a958b4c0
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-01-23 10:47:35 +0200

    add test_model.py

---
+++ tests/test_model.py
@@ -0,0 +1,25 @@
+import ckanext.archiver.model as archiver_model
+try:
+    from ckan.tests.helpers import reset_db
+    from ckan.tests import factories as ckan_factories
+except ImportError:
+    from ckan.new_tests.helpers import reset_db
+    from ckan.new_tests import factories as ckan_factories
+from ckan import model
+import pytest
+
+Archival = archiver_model.Archival
+
+
+class TestArchival(object):
+    @pytest.fixture(autouse=True)
+    def initial_data(self, clean_db):
+        reset_db()
+        archiver_model.init_tables(model.meta.engine)
+
+    def test_create(self):
+        dataset = ckan_factories.Dataset()
+        res = ckan_factories.Resource(package_id=dataset['id'])
+        archival = Archival.create(res['id'])
+        assert isinstance(archival, Archival)
+        assert archival.package_id == dataset['id']
