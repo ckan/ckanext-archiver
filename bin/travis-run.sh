da57d7ad3f041361d0807bb14fc41c7279e70e7c
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-01-23 12:41:46 +0200

    change solr path

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
-sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
+sudo cp ../ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
 
 if [ $CKANVERSION = 'master' ]
