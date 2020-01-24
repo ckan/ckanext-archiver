49a7d408cc4e7173a46fd2461ce3e046e48e2cbf
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-01-24 12:29:37 +0200

    convert tests to pytest

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,9 +1,7 @@
 import ckanext.archiver.model as archiver_model
 try:
-    from ckan.tests.helpers import reset_db
     from ckan.tests import factories as ckan_factories
 except ImportError:
-    from ckan.new_tests.helpers import reset_db
     from ckan.new_tests import factories as ckan_factories
 from ckan import model
 import pytest
